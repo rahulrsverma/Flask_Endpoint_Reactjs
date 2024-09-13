09271a55eff6f51010436f8835beb12251f1c09e
rahulrsverma <rahulverma.22394@gmail.com>
2024-09-13 19:35:32 +0530

    Create Reactpy.py

---
+++ Reactpy.py
@@ -0,0 +1,19 @@
+#Working code
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/data',methods=['GET'])
+def get_data():
+    data = {
+        "message":"Hello this is Get api end point"
+    }
+    return jsonify(data)
+if __name__=='__main__':
+    app.run(host='0.0.0.0',debug=True)
+
+
+
